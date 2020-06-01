7e5bde99054547010b39e9ebbf524fdffdf109be
Charles Guertin <charlesguertin@live.ca>
2020-06-01 13:18:05 -0400

    Added Tilda configuration file setup.

--- config/index.sh
+++ config/index.sh
@@ -6,3 +6,4 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 # Install config files.
 cp $DIR/.zshrc ~/.zshrc
 cp $DIR/vscode_settings.json ~/.config/Code/User/settings.json
+cp $DIR/tilda_config ~/.config/tilda/config_0
