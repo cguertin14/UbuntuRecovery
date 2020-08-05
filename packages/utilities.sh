1598c3566ba359893b958d3b1b5cef425edd927e
Charles Guertin <charlesguertin@live.ca>
2020-08-04 22:55:00 -0400

    Openjdk-11-jdk added to packages/utilities.

--- packages/utilities.sh
+++ packages/utilities.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Utilities
-sudo apt install git curl tig net-tools ansible powertop tlp vagrant bat vim unzip wget tilda neofetch openjdk-8-jdk -y
+sudo apt install git curl tig net-tools ansible powertop tlp vagrant bat vim unzip wget tilda neofetch openjdk-8-jdk openjdk-11-jdk -y
