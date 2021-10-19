291091b52aa21d1c11c2dcad602e4de07f2f2663
Charles Guertin <charlesguertin@live.ca>
2021-10-19 10:15:41 -0400

    Update i3.sh

--- packages/i3.sh
+++ packages/i3.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-sudo apt install feh fonts-font-awesome rofi pulseaudio-utils xbacklight alsa-tools clipit gcc git terminator locate pcmanfm acpi libnotify-bin light nitrogen volumeicon-alsa playerctl
+sudo apt install feh fonts-font-awesome rofi pulseaudio-utils xbacklight alsa-tools \
+                 clipit gcc git terminator locate pcmanfm acpi libnotify-bin light nitrogen \
+                 volumeicon-alsa playerctl i3 \
+                 -y
