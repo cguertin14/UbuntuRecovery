407b32b289683369cb2d67cc07e76916112764bf
Charles Guertin <charlesguertin@live.ca>
2021-10-19 10:17:06 -0400

    Update index.sh

--- config/index.sh
+++ config/index.sh
@@ -3,7 +3,11 @@
 # Current directory.
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+# Create directories
+mkdir -p ~/.config/i3
+
 # Install config files.
+cp $DIR/i3.config ~/.config/i3/config
 cp $DIR/.zshrc ~/.zshrc
 cp $DIR/vscode_settings.json ~/.config/Code/User/settings.json
 cp $DIR/tilda_config ~/.config/tilda/config_0
