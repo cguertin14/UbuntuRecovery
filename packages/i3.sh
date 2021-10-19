0f1a5a33cf62cd7eb8d1f93d28de11e99aa7d4d1
Charles Guertin <charlesguertin@live.ca>
2021-10-19 13:57:35 -0400

    Added blueman for bluetooth UI.

--- packages/i3.sh
+++ packages/i3.sh
@@ -2,5 +2,5 @@
 
 sudo apt install feh fonts-font-awesome rofi pulseaudio-utils xbacklight alsa-tools \
                  clipit gcc git terminator locate pcmanfm acpi libnotify-bin light nitrogen \
-                 volumeicon-alsa playerctl i3 \
+                 volumeicon-alsa playerctl blueman i3 \
                  -y
