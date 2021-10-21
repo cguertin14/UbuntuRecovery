7dc4f807e4e61f2c56146ae272ae672c2061b494
Charles Guertin <charlesguertin@live.ca>
2021-10-21 14:37:27 -0400

    Trigger reload on cpu profile.

--- scripts/index.sh
+++ scripts/index.sh
@@ -6,3 +6,4 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 # Copy scripts to other locations
 sudo cp $DIR/cpufreqctl.sh /usr/local/bin/cpufreqctl
 sudo cp $DIR/cpupowerctl.sh /usr/local/bin/cpupowerctl
+sudo cp $DIR/powersave.rules /etc/udev/rules.d/powersave.rules
