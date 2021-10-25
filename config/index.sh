c8981059fecf37343f65b87610a264a0d3bec63b
Charles Guertin <charlesguertin@live.ca>
2021-10-25 19:52:23 -0400

    Add configuration for logind service

--- config/index.sh
+++ config/index.sh
@@ -15,6 +15,7 @@ cp $DIR/.zshrc ~/.zshrc
 cp $DIR/vscode_settings.json ~/.config/Code/User/settings.json
 cp $DIR/tilda_config ~/.config/tilda/config_0
 cp $DIR/startship.toml ~/.config/startship.toml
+sudo cp $DIR/logind.conf /etc/systemd/logind.conf
 
 # Graphics configuration file
 sudo cp $DIR/graphics.config /etc/X11/xorg.conf.d/20-intel.conf
